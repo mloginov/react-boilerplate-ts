8e5594ea05fc3d19c64aac4167af7379fbd43d33
mikhail.loginov <mloginov@codio.com>
2021-04-11 22:23:21 +0300

    ignore build

---
+++ src/main.d.ts
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { App } from './app';
+export interface IMainProps {
+    app: App;
+}
+export declare class Main extends React.Component<IMainProps, {}> {
+    constructor(props: IMainProps);
+    render(): JSX.Element;
+}
